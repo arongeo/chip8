a38c926049ba3fd5f46596a230151dd518eaa9ee
arongeo <me@arongeo.com>
2023-01-15 13:26:50 +0100

    Updated/added watermarks

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,3 +1,11 @@
+// Copyright 2023 - https://github.com/arongeo
+//
+// Chip-8 Interpreter written in Rust
+// https://github.com/arongeo/chip8
+// 
+// cpu.rs file
+//
+
 #[path = "config.rs"]
 mod config;
 #[path = "chip8.rs"]
